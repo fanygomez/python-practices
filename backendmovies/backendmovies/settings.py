24bec7be5a34d7602cd6161828e3e8c155183324
Stefhani Gomez <fanygomez1894@gmail.com>
2024-12-12 15:55:32 -0600

    chore(dependencies): add django-filters package for advanced API filtering
        - Installed django-filters package to enable sophisticated filtering
        - Integrated with Django REST Framework ViewSet
        - Provides granular filtering capabilities for Film model
        - Supports complex query parameter filtering
    
        Dependencies updated:
        - Added django-filters to project requirements
        - Enables advanced filtering mechanisms for API endpoints

--- backendmovies/backendmovies/settings.py
+++ backendmovies/backendmovies/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'django_rest_passwordreset',
+    'django_filters',
     # Django custom apps
     'authentication',
     'films'
--- backendmovies/films/views.py
+++ backendmovies/films/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Film, FilmGenre
 from .serializers import FilmSerializer, FilmGenreSerializer
 
@@ -7,6 +8,15 @@ from .serializers import FilmSerializer, FilmGenreSerializer
 class FilmViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
+    
+    filter_backends = [DjangoFilterBackend,  filters.SearchFilter, filters.OrderingFilter]
+
+    search_fields = ['title', 'year', 'genres__name']
+    ordering_fields = ['title', 'year', 'genres__name']
+    filterset_fields = {
+        'year': ['lte', 'gte'],  # year is less than or equal to, year is greater than or equal to
+        'genres': ['exact']
+    }
 
 
 class GenreViewSet(viewsets.ReadOnlyModelViewSet):
